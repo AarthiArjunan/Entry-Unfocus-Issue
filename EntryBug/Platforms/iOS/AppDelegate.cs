0bf7bfca1fb26829a809a9fe62394725de1002ce
AARTHI <aarthi.sf3966@gmail.com>
2024-08-06 13:21:28 +0530

    Added sample project

---
+++ EntryBug/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace EntryBug
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
